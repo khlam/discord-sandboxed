9a3ba3f5e0a858f2c344c788aeb4bec16cb1e016
khlam <4841220+khlam@users.noreply.github.com>
2021-04-21 16:23:54 -0700

    discord api v9

--- views/js/mainRender.js
+++ views/js/mainRender.js
@@ -71,14 +71,14 @@ onload = () => {
     const whiteList = [
         'PATCH',                                       // Mute/Unmute/notification/cosmetic guild changes
         'DELETE',                                      // Leaving a guild / Deleting messages
-        'https://discord.com/api/v8/channels/',        // Text channel address
-        'https://discord.com/api/v8/auth/login',       // Login address
-        'https://discord.com/api/v8/invites/',         // Accepting guild invite
-        'https://discord.com/api/v8/voice/regions',    // Required when creating new guild
-        'https://discord.com/api/v8/guilds',           // Creating a guild
-        'https://discord.com/api/v8/gateway',         // This may be required to get past login screen if not cached locally
-        'https://discord.com/api/v8/applications/',
-        'https://discord.com/api/v8/users/'
+        'https://discord.com/api/v9/channels/',        // Text channel address
+        'https://discord.com/api/v9/auth/login',       // Login address
+        'https://discord.com/api/v9/invites/',         // Accepting guild invite
+        'https://discord.com/api/v9/voice/regions',    // Required when creating new guild
+        'https://discord.com/api/v9/guilds',           // Creating a guild
+        'https://discord.com/api/v9/gateway',         // This may be required to get past login screen if not cached locally
+        'https://discord.com/api/v9/applications/',
+        'https://discord.com/api/v9/users/'
     ]
 
     const _whiteList = convertObjToString(whiteList)
